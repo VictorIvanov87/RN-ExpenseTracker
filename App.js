47c5391b8a63df8c2f30e66886d5ed4c0f8efcbf
Victor Hristov <victor.hristov@mentormate.com>
2023-10-23 18:36:13 +0300

    Create main screens, add navigation

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+import { GLOBAL_STYLES } from "./constants/styles";
+import ManageExpense from "./screen/ManageExpense";
+import RecentExpense from "./screen/RecentExpense";
+import AllExpenses from "./screen/AllExpenses";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerStyle: { backgroundColor: GLOBAL_STYLES.colors.primary500 },
+        tabBarStyle: { backgroundColor: GLOBAL_STYLES.colors.primary500 },
+        tabBarActiveTintColor: GLOBAL_STYLES.colors.accent500
+      }}
+    >
+      <BottomTabs.Screen
+        name="RecentExpense"
+        component={RecentExpense}
+        options={{
+          title: "Recent Expense",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => <AntDesign color={color} size={size} name="clockcircleo" />
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color, size }) => <AntDesign color={color} size={size} name="calendar" />
+        }}
+      />
+    </BottomTabs.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} options={{ headerShown: false }} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ screen/RecentExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const RecentExpense = () => {
+  return (
+    <View>
+      <Text>RecentExpense</Text>
+    </View>
+  );
+};
+
+export default RecentExpense;
